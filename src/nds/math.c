69788c240a9b2c5b45703e4e7a871d0aafb38b11
Kuratius <Kuratius@gmx.net>
2024-04-29 19:47:23 +0200

    rename sqrtf file, remove div and mul implementations

--- src/nds/math.c
+++ src/nds/math.c
@@ -1,10 +1,11 @@
 
-#include "nds_include.h" 
-
+#include "nds.h" 
 #define f32 float
 #define u32 uint32_t
 #define s32 int32_t
 #define u64 uint64_t
+
+
 
 #ifdef TARGET_NDS
 #define NDS_ITCM_CODE __attribute__((section(".itcm")))
@@ -12,23 +13,35 @@
 #define NDS_ITCM_CODE
 #endif
 
-NDS_ITCM_CODE f32 fsqrt(f32 x){
+
+
+
+
+NDS_ITCM_CODE f32 __wrap_sqrtf(f32 x){
     union{f32 f; u32 i;}xu;
     xu.f=x;
-    //grab exponent
-    s32 exponent= (xu.i & (0xff<<23));
-    if(exponent==0)return 0.0;
-    exponent=exponent-(127<<23);
-    exponent=exponent>>1; //right shift on negative number depends on compiler
     u64 mantissa=xu.i & ((1<<23)-1);
-    mantissa=(mantissa+(1<<23))<<23;
-    if ((exponent & (1<<22))>0){
-    mantissa=mantissa<<1;
-    }
-    //printf("exponent: %d\n", exponent);
-    //printf("mantissa: %ld\n", mantissa);
-    u32 new_mantissa= (u32) sqrt64(mantissa); //modify this line to use hardware sqrt
+    //check if exponent is odd
+    //before subtracting 127 exponent was even if it is odd now
+    //therefore check if last digit is 0 
+    mantissa=(mantissa+(1<<23))<<   ( (( xu.i & (1<<23))==0  )      +25 );
+    REG_SQRTCNT = SQRT_64;
+    REG_SQRT_PARAM = mantissa;
+    u32 raw_exponent= (xu.i & (0xff<<23));
+    if (raw_exponent > 0 ) {
+	s32 exponent=raw_exponent-(127<<23);
+	exponent=exponent>>1; //right shift on negative number depends on compiler
+	exponent=((exponent+(127<<23))& (0xff<<23) );
+    //fetch async result here
+	while(REG_SQRTCNT & SQRT_BUSY);
+	u64 new_mantissa=REG_SQRT_RESULT;
+	new_mantissa+=1;
+	new_mantissa>>=1;
+	xu.i=  exponent| (new_mantissa & ((1<<23)-1));
+	return xu.f;
+    } else{
+	while(REG_SQRTCNT & SQRT_BUSY);
+	return 0.0;
+    };
 
-    xu.i= ((exponent+(127<<23))& (0xff<<23) ) | (new_mantissa & ((1<<23)-1));
-    return xu.f;
 }
